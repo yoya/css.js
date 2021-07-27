2b8d8586b74dc1dc0341e49f4a817958a1b7d630
Yoshihiro Yamazaki <yoya@awm.jp>
2021-07-28 00:31:40 +0900

    support filter, sepia & blur

--- main.js
+++ main.js
@@ -11,8 +11,29 @@ function cssFilterMain() {
     const srcImage = document.getElementById("srcImage");
     const dstImage = document.getElementById("dstImage");
     const dstContainer = document.getElementById("dstContainer");
+    const filterSelect = document.getElementById("filterSelect");
+    const filterRange = document.getElementById("filterRange");
+    let filter = filterSelect.value;
     dropFunction(document, function(file) {
-        srcImage.src = file;
-        dstImage.src = file;
+        srcImage.src = dstImage.src = file;
+    });
+    filterSelect.addEventListener("change", function(e) {
+        filter = e.target.value;
+    });
+    filterRange.addEventListener("input", function(e) {
+        const v = e.target.value;
+        let filterValue = "";
+        switch (filter) {
+        case "sepia":
+            filterValue = "sepia("+v+"%)";
+            break;
+        case "blur":
+            const vv = v/10;
+            filterValue = "blur("+vv+"px)";
+            break;
+        default:
+            console.error("unknown filter type:"+filter);
+        }
+        dstContainer.style["filter"] =  filterValue;
     });
 }
