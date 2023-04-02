2d247f1921629de03a22aa897aaeedfd75858722
Yoshihiro Yamazaki <yoya@awm.jp>
2023-04-02 20:53:46 +0900

    upload image for border-radius demo.

--- border-radius.js
+++ border-radius.js
@@ -2,6 +2,11 @@
 
 console.log("main")
 
+function roundByDecimalDigit(n, d) {
+    const scale = 10**d;
+    return String(Math.round(n * scale) / scale);
+}
+
 function setStyle(target) {
     const aspected = aspectedCheckbox.checked;
     const round = Number(target.value);
@@ -9,13 +14,13 @@ function setStyle(target) {
     if (aspected) {
         const value = String(round * 50) + "%";
         image.style.borderRadius = value;
-        radiusText.innerText = "border-radius:" + value;
+        radiusText.innerText = "border-radius: " + value;
     } else {
         const { width, height }  = image;
         const radius = Math.min(width, height) / 2 * round;
-        const value = String(radius) + "px";
+        const value = roundByDecimalDigit(radius, 3)+ "px";
         image.style.borderRadius = value;
-        radiusText.innerText = "border-radius:" + value;
+        radiusText.innerText = "border-radius: " + value;
     }
 }
 
@@ -25,6 +30,21 @@ roundRange.addEventListener("input", function(e) {
 
 aspectedCheckbox.addEventListener("input", function(e) {
     setStyle(roundRange);
+});
+
+fileSelect.addEventListener("input", function(e) {
+    const idx = fileSelect.selectedIndex;
+    image.src = fileSelect.options[idx].value;
+    image.onload = (e) => {
+        setStyle(roundRange);
+    }
+});
+
+dropFunction(document, function(file) {
+    image.src = file;
+    image.onload = (e) => {
+        setStyle(roundRange);
+    }
 });
 
 setStyle(roundRange);
